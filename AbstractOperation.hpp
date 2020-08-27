df32c214cdc56d61868313e9d14cf224a71e0365
tomer12901@gmail.com <tomer12901@gmail.com>
2020-08-27 18:24:58 +0300

    added source and headers files of AbstractOperation

---
+++ AbstractOperation.hpp
@@ -0,0 +1,38 @@
+#include <string>
+
+namespace AbstractOperation{
+    class AbstractOperation{
+    private:
+        std::string m_operation;
+        std::string m_inputFiles;
+        std::string m_outputFile; 
+        CacheManager m_cacheManager;
+
+
+    public:
+            /**
+             * @brief Construct a new Abstract Operation object
+             * 
+             * @param m_operation - operation
+             * @param m_inputFiles - input files of operation
+             * @param m_outputFile - output file 
+             * @param m_cacheManager - cache manager object
+             */
+           AbstractOperation(std::string m_operation, std::string m_inputFiles, std::string m_outputFile, CacheManager m_cacheManager);
+
+            /**
+             * @brief The function check if the parameters are appopriate
+             * 
+             * @return true - parameters are appopriate
+             * @return false - parameters are not appopriate
+             */
+            virtual bool checkParameters() const = 0;
+
+            /**
+             * @brief The function do the operation of the user
+             * 
+             */
+            virtual void doOperation() const = 0;
+
+    };
+}
